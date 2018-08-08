8eda058e159d6f190c1abd19f23ff407cdb8a3bb
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-08 09:26:53 -0500

    import Portal component

--- src/Modal.js
+++ src/Modal.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import Portal from './Portal';
 
 export default class Modal extends Component {
   render() {
-    return <div />;
+    return <Portal />;
   }
 }
